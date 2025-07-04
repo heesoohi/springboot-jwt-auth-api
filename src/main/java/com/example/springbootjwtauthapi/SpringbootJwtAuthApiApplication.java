8f121bcbd95282d22797ac47c2661ced08df9c32
이희수 <leehsdd33@gmail.com>
2025-07-04 16:28:28 +0900

    chore: Spring Boot + JWT Auth API setup start

---
+++ src/main/java/com/example/springbootjwtauthapi/SpringbootJwtAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjwtauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtAuthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJwtAuthApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootjwtauthapi/SpringbootJwtAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjwtauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtAuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
